d60cae1a3e208c43e7ac31d401a8bd908b662e86
Marko <marko.rueterbories@consultingwerk.de>
2024-08-28 12:33:20 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ScannerTest';
+
+  strEnteredText: string = "";
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    //this.key = event.key;
+    this.strEnteredText = this.strEnteredText + event.key;
+    
+    this.handleScannedValues('UDI;');
+    this.handleScannedValues('GTIN;');
+    this.handleScannedValues('WBPLO;');
+    this.handleScannedValues('WBPLO;');
+  }
+
+  handleScannedValues(ident: string) {
+    if (this.strEnteredText.includes(ident)) {
+      var identStartIndex: number = this.strEnteredText.indexOf(ident);
+
+      console.log("ident: " + ident + "found at start position: " + identStartIndex);
+
+      if (identStartIndex > 0) {
+        console.log(this.strEnteredText);
+
+        this.strEnteredText = this.strEnteredText.slice(identStartIndex);
+
+        console.log(this.strEnteredText);
+      } else {
+        if (identStartIndex == 0) {
+          var identEndIndex: number = this.strEnteredText.indexOf(ident, 3);
+
+          if (identEndIndex > 0) {
+            console.log("full string for ident " + ident + " was found: " + this.strEnteredText);
+          }
+
+        }
+      }
+    }
+  }
 }
+ 
+
